5a1b32b07f8b90314b5f1e4fbea6af05cb216717
Joshua <19979299+Zooney-17@users.noreply.github.com>
2023-04-13 00:22:31 -0400

    Create a 16x16 grid on the page

---
+++ script.js
@@ -0,0 +1,9 @@
+const gridContainer = document.querySelector('#grid-container')
+
+for (let i = 0; i <= 255; i++) {
+    const grid = document.createElement('div');
+    grid.style.width = '55px';
+    grid.style.height = '55px';
+    grid.style.border = '1px solid black';
+    gridContainer.appendChild(grid);
+}
